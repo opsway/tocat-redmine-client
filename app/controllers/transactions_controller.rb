a977fc25fe788f091791fce2bafd4190ec4df21f
Alex Gornov <zmokizmoghi@gmail.com>
2015-05-21 19:10:07 +0700

    [[##27117]]

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,21 +7,29 @@ class TransactionsController < ApplicationController
   before_filter :check_action
 
   def index
-    sort_update %w(id comment total)
+    sort_update %w(created_at comment total)
 
     query_params = {}
     query_params[:limit] = params[:per_page] if params[:per_page].present?
     query_params[:page] = params[:page] if params[:page].present?
     query_params[:search] = params[:search] if params[:search].present?
-    query_params[:user] = params[:user] if params[:user].present?
-    query_params[:team] = params[:team] if params[:team].present?
+    if params[:owner].present?
+      if params[:owner].split('_').first == 'group'
+        query_params[:team] = params[:owner].split('_').second.to_i
+      elsif params[:owner].split('_').first == 'user'
+        query_params[:user] = params[:owner].split('_').second.to_i
+      end
+    end
+    query_params[:search] = "#{query_params[:search]} account == #{params[:account_type]}" if params[:account_type].present?
     query_params[:sort] = params[:sort] if params[:sort].present?
 
     @transactions = TocatTransaction.all(params: query_params)
     @transactions_count = @transactions.http_response['X-total'].to_i
     @transactions_pages = Paginator.new self, @transactions_count, @transactions.http_response['X-Per-Page'].to_i, params['page']
-    @teams = TocatTeam.all
-    @users = TocatUser.all
+    @owners = {
+      Group: TocatTeam.all.collect { |r| [r.name, "group_#{r.id}"] },
+      User: TocatUser.all.collect { |r| [r.name, "user_#{r.id}"] }
+    }
   end
 
   private
