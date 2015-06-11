242033182c9d6108c1401df79308abace407e5d1
Alex Gornov <zmokizmoghi@gmail.com>
2015-06-11 22:24:58 +0700

    productivity chart fixes

--- app/controllers/tocat_controller.rb
+++ app/controllers/tocat_controller.rb
@@ -199,41 +199,56 @@ class TocatController < ApplicationController
     begin
       @user_tocat = TocatUser.find(TocatUser.find_by_name(@user.name).id)
       @team_tocat = TocatTeam.find(@user_tocat.team.id)
-      transactions = TocatTransaction.get_transactions_for_user(@user_tocat.id)
-      @balance_transactions = []
-      @income_transactions = []
-      transactions.each do |t|
-        t.type == 'balance' ?
-          @balance_transactions << t :
-          @income_transactions << t
+      if @user_tocat.role == 'Manager'
+        @team_income_transactions = TocatTransaction.find(:all, params:{team: @team_tocat.id, limit:9999999, search: "account = income" })
+      else
+        @team_balance_transactions = TocatTransaction.find(:all, params:{team: @team_tocat.id, limit:9999999, search: "account = balance" })
       end
-      transactions = TocatTransaction.get_transactions_for_team(@team_tocat.id)
-      @team_balance_transactions = []
-      @team_income_transactions = []
-      transactions.each do |t|
-        t.type == 'balance' ?
-          @team_balance_transactions << t :
-          @team_income_transactions << t
-      end
-      @balance_chart = { balance: [], forecast: [], zero_line: [], timeline: [] }
+      @income_transactions = TocatTransaction.find(:all, params:{user: @user_tocat.id, limit:9999999, search: "account = payment" })
+      @balance_transactions = TocatTransaction.find(:all, params:{user: @user_tocat.id, limit:9999999, search: "account = balance" })
+      @balance_chart = { week: { balance: [], forecast: [], zero_line: [], timeline: [] },
+                         month: { balance: [], forecast: [], zero_line: [], timeline: [] },
+                         halfyear: { balance: [], forecast: [], zero_line: [], timeline: [] },
+                         year: { balance: [], forecast: [], zero_line: [], timeline: [] } }
       balance_transactions_ = TocatTransaction.find(:all, params: { search: "accountable_type == User accountable_id == #{@user_tocat.id} created_at >= #{1.year.ago.strftime('%Y-%m-%e')} account = balance", limit: 9999999})
       accepted_not_paid = TocatTicket.find(:all, params: { search: "resolver == #{@user_tocat.id} accepted == 1 paid == 0", limit: 99999 })
       accepted_not_paid_events = TocatTicket.events_for(accepted_not_paid.collect(&:id), "task.accepted_update")
       balance_with_tasks = balance =  @user_tocat.balance_account_state - balance_transactions_.sum { |r| r.total.to_i}
+      week = (1.week.ago.to_date..Date.today)
+      month = (1.month.ago.to_date..Date.today)
+      halfyear = (6.months.ago.to_date..Date.today)
+      year = (1.year.ago.to_date..Date.today)
+      events_count = accepted_not_paid_events.select{ |r| r.parameters['new'] == true }.count
       (1.year.ago.to_date..Date.today).each do |date|
         events_sum = accepted_not_paid_events.select{ |r| r.created_at.to_date == date && r.parameters['new'] == true }.sum { |r| r.parameters["balance"].to_i}
         transactions_sum =  @balance_transactions.select{ |r| r.date.to_date == date}.sum { |r| r.total.to_i }
-        @balance_chart[:balance] << (balance += transactions_sum).round(2)
-        @balance_chart[:forecast] << (balance_with_tasks += (events_sum + transactions_sum)).round(2)
-        @balance_chart[:zero_line] << 0
-        @balance_chart[:timeline] << date
+        balance_with_transactions = (balance += transactions_sum).round(2)
+        forecast_balance = (balance_with_tasks += (events_sum + transactions_sum)).round(2)
+        if week.include?(date)
+          @balance_chart[:week][:balance] << balance_with_transactions
+          @balance_chart[:week][:forecast] << forecast_balance if events_count == accepted_not_paid.count
+          @balance_chart[:week][:zero_line] << 0
+          @balance_chart[:week][:timeline] << date
+        end
+        if month.include?(date)
+          @balance_chart[:month][:balance] << balance_with_transactions
+          @balance_chart[:month][:forecast] << forecast_balance if events_count == accepted_not_paid.count
+          @balance_chart[:month][:zero_line] << 0
+          @balance_chart[:month][:timeline] << date
+        end
+        if halfyear.include?(date)
+          @balance_chart[:halfyear][:balance] << balance_with_transactions
+          @balance_chart[:halfyear][:forecast] << forecast_balance if events_count == accepted_not_paid.count
+          @balance_chart[:halfyear][:zero_line] << 0
+          @balance_chart[:halfyear][:timeline] << date
+        end
+        @balance_chart[:year][:balance] << balance_with_transactions
+        @balance_chart[:year][:forecast] << forecast_balance if events_count == accepted_not_paid.count
+        @balance_chart[:year][:zero_line] << 0
+        @balance_chart[:year][:timeline] << date
       end
       @accepted_tasks = TocatTicket.get_accepted_tasks(true, @user_tocat.id)
       @not_accepted_tasks = TocatTicket.get_accepted_tasks(false, @user_tocat.id)
-      @not_accepted_balance = 0
-      @not_accepted_tasks.each { |t| @not_accepted_balance += t.budget }
-      @accepted_balance = 0
-      @accepted_tasks.each { |t| @accepted_balance += t.budget }
     rescue Exception => e
       return render_404
     end
