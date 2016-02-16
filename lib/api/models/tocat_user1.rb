117a42e81657c75484ff7e984345902ed32a1208
Vitaliy <vbykovec@gmail.com>
2016-02-16 11:18:36 +0200

    simplify tocat_user1 model

--- lib/api/models/tocat_user1.rb
+++ lib/api/models/tocat_user1.rb
@@ -1,21 +1,7 @@
 require 'active_resource'
 class TocatUser1 < ActiveResource::Base
   unloadable
-  include ActiveModel::Validations
   self.site = RedmineTocatClient.settings[:host]
   self.collection_name = 'users'
   self.element_name = 'user'
-  #self.include_root_in_json = true
-  schema do
-    attribute 'id', :integer
-    attribute 'login', :string
-    attribute 'name', :string
-    attribute 'team', :integer
-    attribute 'role', :integer
-    decimal 'daily_rate'
-  end
-  validates :login, :name, :team, :role, :daily_rate, presence: true
-  def to_s
-    self.name
-  end
 end
