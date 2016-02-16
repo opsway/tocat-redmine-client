c7aaf958676f16d4090a132ddf89de466749b99b
Vitaliy <vbykovec@gmail.com>
2016-02-16 13:03:19 +0200

    fix bug

--- lib/api/models/track.rb
+++ lib/api/models/track.rb
@@ -2,6 +2,6 @@ require 'active_resource'
 class TocatTrack < ActiveResource::Base
   unloadable
   self.site = RedmineTocatClient.settings[:host]
-  self.collection_name = 'tocat_users
+  self.collection_name = 'tocat_users'
   self.element_name = 'tocat_user'
 end
