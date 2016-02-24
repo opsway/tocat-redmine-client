f4edc6621cfc6fd397a7557d080ccfea4f770bbc
Vitaliy <vbykovec@gmail.com>
2016-02-24 12:47:07 +0200

    fix client error (args)

--- lib/patches/users_helper.rb
+++ lib/patches/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
   unloadable
   alias_method :old_user_settings_tabs, :user_settings_tabs
   def user_settings_tabs(user=nil)
-    tabs = old_user_settings_tabs(user)
+    tabs = old_user_settings_tabs #(user)
     tabs << {:name => 'tocat', :partial => 'tocat_roles/tocat', :label => :label_tocat_role}
     tabs
   end
