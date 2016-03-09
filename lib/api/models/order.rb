92b050edcb31f5e7e75284500335f91a3cf4bf46
vlaew <vlaew5@gmail.com>
2016-03-09 14:53:53 +0300

    Add autocomplete for parent order in order edit page

--- lib/api/models/order.rb
+++ lib/api/models/order.rb
@@ -17,12 +17,18 @@ class TocatOrder < ActiveResource::Base
       query_options.merge!({:current_user => User.current.name})
       "#{prefix(prefix_options)}#{collection_name}#{query_string(query_options)}"
     end
+
+    def auto_complete(term)
+      response = self.get(:auto_complete, { term: term })
+      instantiate_collection(response)
+    end
   end
 
 
   schema do
     attribute 'id', :integer
     attribute 'parent_order', :string
+    attribute 'parent_id', :integer
     attribute 'name', :string
     attribute 'description', :text
     attribute 'team', :string
