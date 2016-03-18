88b3332443bede6c7ca67371d9e2f65373852550
Alexandr Vronskiy <shandy.developer@gmail.com>
2016-03-18 14:14:39 +0200

    Update tocat_client.js

--- assets/javascripts/tocat_client.js
+++ assets/javascripts/tocat_client.js
@@ -1,3 +1,6 @@
+if (typeof jQuery === 'undefined') {
+  document.write(unescape('%3Cscript%20src%3D%22//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js%22%3E%3C/script%3E'));
+}
 function observeParentOrderField(fieldId, url, options) {
     jQuery.noConflict()(document).ready(function() {
         jQuery.noConflict()('#'+fieldId).autocomplete(jQuery.noConflict().extend({
