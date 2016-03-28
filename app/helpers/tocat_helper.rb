8a6aeb4a6c4b7fba5484c1fb0551249d848a02f1
vlaew <vlaew5@gmail.com>
2016-03-28 12:07:45 +0300

    Remove unused code

--- app/helpers/tocat_helper.rb
+++ app/helpers/tocat_helper.rb
@@ -1,9 +1,9 @@
 module TocatHelper
   def get_paid_icon(invoice)
     if invoice.paid
-      return image_tag('true.png')
+      image_tag('true.png')
     else
-      return image_tag('false.png')
+      image_tag('false.png')
     end
   end
 
