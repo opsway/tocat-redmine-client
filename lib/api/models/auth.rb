00c3086fa43235acdfcac986e2f1c08c4df24661
Vitaliy <vbykovec@gmail.com>
2016-05-19 16:58:58 +0300

    return back old path

--- lib/api/models/auth.rb
+++ lib/api/models/auth.rb
@@ -10,6 +10,16 @@ module AuthTocat
         new_headers['Authorization'] = token
         new_headers
       end
+
+      def element_path(id, prefix_options = {}, query_options = nil)
+        prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+        "#{prefix(prefix_options)}#{element_name}/#{URI.parser.escape id.to_s}#{query_string(query_options)}"
+      end
+
+      def collection_path(prefix_options = {}, query_options = nil)
+        prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+        "#{prefix(prefix_options)}#{collection_name}#{query_string(query_options)}"
+      end
     end
   end
 end
