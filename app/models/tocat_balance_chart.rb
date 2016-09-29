b5e890f72e449a08d0771e978b6bba11fb7204c8
Vitaliy <vbykovec@gmail.com>
2016-09-29 09:32:21 +0300

    fix balance chart

--- app/models/tocat_balance_chart.rb
+++ app/models/tocat_balance_chart.rb
@@ -47,7 +47,7 @@ class TocatBalanceChart
       "created_at >= #{period.begin.strftime('%Y-%m-%d')}",
       "created_at <= #{period.end.strftime('%Y-%m-%d')}"
     ].join(' ')
-    TocatTransaction.find(:all, params: { user: tocat_user.id, search: search, limit: TRANSACTIONS_LIMIT })
+    TocatTransaction.find(:all, params: { user: tocat_user.accounts.balance.id, search: search, limit: TRANSACTIONS_LIMIT })
   end
 
   def balance_transactions(tocat_user, period)
@@ -64,7 +64,7 @@ class TocatBalanceChart
       "created_at <= #{period.end.strftime('%Y-%m-%d')}",
       'account = balance'
     ].join(' ')
-    TocatTransaction.find(:all, params: { user: tocat_user.id, search: search, limit: TRANSACTIONS_LIMIT })
+    TocatTransaction.find(:all, params: { user: tocat_user.accounts.balance.id, search: search, limit: TRANSACTIONS_LIMIT })
   end
 
   class PresetPeriods
