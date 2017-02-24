0f1b26c706b37bc6797739601362ee1869b59a22
Denys Kulyk <morequalified@gmail.com>
2017-02-24 15:08:55 +0200

    rebuild pay in full

--- app/controllers/external_payments_controller.rb
+++ app/controllers/external_payments_controller.rb
@@ -2,18 +2,8 @@
 class ExternalPaymentsController < TocatBaseController
   unloadable
   before_filter :check_action
-  before_filter :find_request, except: [:index,:create,:new,:salary_checkin, :pay_in_cash, :pay_in_full]
+  before_filter :find_request, except: [:index,:create,:new,:salary_checkin, :pay_in_cash]
   around_filter :process_errors_and_render, only: [:approve, :cancel, :reject, :complete]
-
-  def pay_in_full
-    payment_request = PaymentRequest.pay_in_full params[:account_id]
-    if payment_request.present?
-      flash[:notice] = l(:notice_payment_request_successful_create)
-    else
-      flash[:notice] = t('Failed with: ', error: error)
-    end
-    return redirect_back_or_default(action: 'my_tocat', controller: 'tocat')
-  end
 
   def index
     query_params = {}
